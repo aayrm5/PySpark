e3b8739aa139ee76ff48b463639fcfb634f3bb27
Riz <aayrm5@gmail.com>
2022-02-06 22:58:38 +0530

    DataFrame Reader/Writer API - Parquet, WriteModes,Repartitioning, Duplicates, Dropping

--- week12/_1.py
+++ week12/_1.py
@@ -15,30 +15,30 @@ spark = (SparkSession
     .getOrCreate()
 )
 
-# order_schema = StructType([
-#     StructField("order_id", IntegerType()),
-#     StructField("order_date", TimestampType()),
-#     StructField("order_customer", IntegerType()),
-#     StructField("order_status", StringType())
-# ])
+order_schema = StructType([
+    StructField("order_id", IntegerType()),
+    StructField("order_date", TimestampType()),
+    StructField("order_customer", IntegerType()),
+    StructField("order_status", StringType())
+])
 
-# order_df = (
-#     spark.read
-#     .option("header", True)
-#     .schema(order_schema)
-#     .csv("../week 9 - Spark1/orders-201019-002101.csv")
-# )
+order_df = (
+    spark.read
+    .option("header", True)
+    .schema(order_schema)
+    .csv("../week 9 - Spark1/orders-201019-002101.csv")
+)
 
-# print(f"number of partitions:= {order_df.rdd.getNumPartitions()}")
+print(f"number of partitions:= {order_df.rdd.getNumPartitions()}")
 
-# order_df = order_df.withColumn("order_OnlyDate", F.to_date(F.col("order_date"), 'yyyy-MM-dd'))
+order_df = order_df.withColumn("order_OnlyDate", F.to_date(F.col("order_date"), 'yyyy-MM-dd'))
 
-# order_rep = order_df.repartition(4)
+order_rep = order_df.repartition(4)
 
-# (order_rep.write
-#     .mode("overwrite")
-#     .parquet("../outputFolder/orders_parquet")
-# )
+(order_rep.write
+    .mode("overwrite")
+    .parquet("../outputFolder/orders_parquet")
+)
 
 
 order_parq_df = (
